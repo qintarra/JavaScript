602c4956dbeb2e85d2837fb117ea3a980b1ec5eb
Qintarra <brain_damage@ukr.net>
2022-05-02 16:10:09 +0300

    modified

--- hw2-1.js
+++ hw2-1.js
@@ -28,48 +28,7 @@ const age_3 = 60
 // }
 // Print to the console the result of the function with ages 17, 18, 61
 
-// const checkAge = function(age) {
-//   if (age < age_2){
-//     console.log('You don’t have access cause your age is ' + age + '. It’s less then ' + age_2)
-//   } else if (age >= age_2 && age < age_3){
-//     console.log('Welcome!')
-//   } else if (age > age_3){
-//     console.log('Keep calm and watch Discovery channel')
-//   } else {
-//     console.log('Technical work')
-//   }
-// }
-
-// checkAge(17)
-// checkAge(18)
-// checkAge(61)
-
-// 2*:
-// Convert task 1* that the data type is checked first in the function. And if it's not a Number, generates an error message.
-
-// const checkAge = function(age) {
-//   if (typeof age == 'number') {
-
-//   if (age < age_2) {
-//     console.log('You don’t have access cause your age is ' + age + '. It’s less then ' + age_2)
-//   } else if (age >= age_2 && age < age_3) {
-//     console.log('Welcome!')
-//   } else if (age > age_3) {
-//     console.log('Keep calm and watch Discovery channel')
-//   } else {
-//     console.log('Technical work')
-//   } 
-// } else { console.log('Error: please, enter number')}
-// }
-
-// checkAge(18)
-// checkAge('twenty')
-
-// 3**:
-// Convert task 2* in such a way that the value '2' (the string containing ONLY NUMBER) is skipped, converted to number
-
 const checkAge = function(age) {
-  if (+age){
   if (age < age_2){
     console.log('You don’t have access cause your age is ' + age + '. It’s less then ' + age_2)
   } else if (age >= age_2 && age < age_3){
@@ -78,10 +37,9 @@ const checkAge = function(age) {
     console.log('Keep calm and watch Discovery channel')
   } else {
     console.log('Technical work')
-  } 
-} else { console.log('Error: please, enter number')}
+  }
 }
 
 checkAge(17)
-checkAge('19')
-checkAge('twenty')
+checkAge(18)
+checkAge(61)
