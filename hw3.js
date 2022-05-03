3639fdacdfc63a4aa3bffc61e0f45a10a21c78f8
Qintarra <brain_damage@ukr.net>
2022-05-03 12:19:24 +0300

    add task 2

--- hw3.js
+++ hw3.js
@@ -38,3 +38,39 @@ console.log(power3(10))
 
 
 
+// 2. Write a script that will output 5 lines to the console in such a way that :) is displayed on the first line, :) :) on the second line, and so on
+// Example in console:
+// :)
+// :):)
+// :):):)
+// :):):):)
+// :):):):):)
+
+let word = ':)'
+let result2 = ''
+for (let i=1; i<=4;i++) {
+result2+=word
+console.log(result2)
+}
+
+
+// 2*. Convert task 2 into a function that takes a string as input, which will be output to the console (as in the smiley condition), as well as the number of lines to output
+// e.g. function printSmile(stroka, numberOfRows)
+
+//Option 1
+function printSmile(stroka, numberOfRows) {
+    let result = '';
+    for (let i = 1; i<=numberOfRows; i++){
+        result+=stroka
+        console.log(result)   
+    }
+}
+printSmile(':(', 5)
+
+//Option 2
+function printSmile2(stroka, numberOfRows) {
+    for (let i = 1; i<=numberOfRows; i++){
+        console.log(stroka.repeat(i))  
+    }
+}
+printSmile2('=)', 6)
