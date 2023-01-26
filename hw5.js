5454d1a5f42bbec098e6aa8d84c4d8948318f528
qintarra <96946215+qintarra@users.noreply.github.com>
2023-01-26 09:42:49 +0200

    task 1

---
+++ hw5.js
@@ -0,0 +1,11 @@
+// 1. Create a function that will return the sum of 2 numbers passed to this function and will write it to the console.
+function sum(a, b){
+    let s = a + b
+    console.log(s) 
+    return s 
+}
+
+sum (3, 4)
+sum (-7, 2)
+
+
